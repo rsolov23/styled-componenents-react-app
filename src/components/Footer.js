d2a02c0e734ee12ce06e8d2bf000f696de389613
Rachel Solov <rsolov23@yahoo.com>
2021-11-17 14:42:05 -0600

    added social icons component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "./styles/Container.styled";
 import { Flex } from "./styles/Flex.styled";
 import { StyledFooter } from "./styles/Footer.styled";
+import SocialIcons from "./SocialIcons";
 
 export default function Footer() {
   return (
@@ -27,7 +28,7 @@ export default function Footer() {
             <li>Blog</li>
             <li>Contact Us</li>
           </ul>
-          {/* <SocialIcons /> */}
+          <SocialIcons />
         </Flex>
         <p>&copy; 2021 Huddle. All rights reserved</p>
       </Container>
